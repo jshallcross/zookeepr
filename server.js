2557719547fba1b609b5a319fa3d96b0cbb92143
Jon Shallcross <jshallcross1@icloud.com>
2021-02-15 16:39:34 -0500

    Port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
@@ -43,7 +43,7 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-  
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -52,6 +52,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
